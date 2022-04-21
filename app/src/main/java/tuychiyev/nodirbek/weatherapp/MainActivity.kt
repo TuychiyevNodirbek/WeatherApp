f85bf8893a999d53e25ad5496dacf83c5a1c2b87
JuraevJakhongirkhuja <johongirjoraev4@GMAIL.COM>
2022-04-21 16:18:08 +0500

    added Toast

--- app/src/main/java/tuychiyev/nodirbek/weatherapp/MainActivity.kt
+++ app/src/main/java/tuychiyev/nodirbek/weatherapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package tuychiyev.nodirbek.weatherapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Toast.makeText(this, "Nodir nma gap ", Toast.LENGTH_SHORT).show()
     }
 }
